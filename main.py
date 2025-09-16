8fbcf87b8c2626e71e4521bf0a208ac55c4015e4
Kurfyoo <143193879+Kurfyoo@users.noreply.github.com>
2025-09-16 18:05:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import sys
 import os
 
-print("Hello, I am the Dunking Doughnuts Employment Chatbot, DDEC!")
-
-name = input("What is your name? ")
-age = int(input("How old are you? "))
-
-if (name == "Jeremy" or name == "jeremy") and age == 16:
-    dad = input("Are you my creator? (y/n): ")
-if dad == "y":
-    name = "creator"
-
-if age < 16:
-    print("Welcome, "+name+". You are "+str(16-age)+" year(s) younger than my creator!")
-elif age == 16:
-    print("Welcome, "+name+". My creator's the same age!")
-else:
-    print("Welcome, "+name+". You are "+str(age-16)+" year(s) older than my creator!")
+def intro():
+    print("Hello, I am the Dunking Doughnuts Employment Chatbot, DDEC!")
+def get_info():
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    if (name == "Jeremy" or name == "jeremy") and age == 16:
+        dad = input("Are you my creator? (y/n): ")
+    if dad == "y":
+        name = "creator"
+    return name,age
+def greet_user(name,age):
+    if age < 16:
+        print("Welcome, "+name+". You are "+str(16-age)+" year(s) younger than my creator!")
+    elif age == 16:
+        print("Welcome, "+name+". My creator's the same age!")
+    else:
+        print("Welcome, "+name+". You are "+str(age-16)+" year(s) older than my creator!")
 
 def choice0():
     pass
@@ -32,12 +33,20 @@ def exit():
     sys.exit()
 choices = [choice0, choice1, choice2, choice3, exit]
 
-print("\nHow can I help you?")
-print("0) [placeholder]")
-print("1) [placeholder]")
-print("2) [placeholder]")
-print("3) [placeholder]")
-print("4) exit")
-option = int(input("Enter a number (0-4): "))
+def menu():
+    print("How can I help you?")
+    print("0) [placeholder]")
+    print("1) [placeholder]")
+    print("2) [placeholder]")
+    print("3) [placeholder]")
+    print("4) exit")
+    option = int(input("Enter a number (0-4): "))
+    choices[option]()
 
-choices[option]()
+def main():
+    intrro()
+    name,age = get_info()
+    greet_user(name,age)
+    menu()
+if __name__ == "__main__":
+    main()
