4562dd577db86d67d35bce21f550f43cf9024a18
Kurfyoo <143193879+Kurfyoo@users.noreply.github.com>
2025-09-17 22:22:10 +0000

    debugging and added waiting mechanism for the user

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import sys
 import os
+import refunc as rf
 
 def intro():
     print("Hello, I am the Dunking Doughnuts Employment Chatbot, DDEC!")
 def get_info():
     name = input("What is your name? ")
     age = int(input("How old are you? "))
-    if (name == "Jeremy" or name == "jeremy") and age == 16:
-        dad = input("Are you my creator? (y/n): ")
-    if dad == "y":
-        name = "creator"
     return name,age
 def greet_user(name,age):
     if age < 16:
@@ -28,10 +25,10 @@ def choice3():
     pass
 def exit():
     os.system("clear")
-    print("Glad if I could help "+name+". Goodbye!")
+    print("Glad if I could help. Goodbye!")
     sys.exit()
 choices = [choice0, choice1, choice2, choice3, exit]
-def menu():
+def menu(name,age):
     print("How can I help you?")
     print("0) [placeholder]")
     print("1) [placeholder]")
@@ -39,11 +36,19 @@ def menu():
     print("3) [placeholder]")
     print("4) exit")
     option = int(input("Enter a number (0-4): "))
+    os.system("clear")
     choices[option]()
+    return option
 def main():
-    intrro()
+    intro()
+    rf.wait()
     name,age = get_info()
+    rf.wait()
     greet_user(name,age)
-    menu()
+    rf.wait()
+    choice = None
+    while choice != 4:
+        choice = menu(name,age)
+        rf.wait()
 if __name__ == "__main__":
     main()
